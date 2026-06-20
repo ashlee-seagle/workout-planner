7e505888ce195bb9b6565dad9910a4a41a662619
ashlee-seagle <ashlee-seagle10@gmail.com>
2026-06-20 10:31:32 -0400

    Convert GoalSelector to TypeScript

--- src/GoalSelector.tsx
+++ src/GoalSelector.tsx
@@ -1,9 +1,20 @@
-function GoalSelector({ goals, selectedGoals, onToggleGoal }) {
+type Goal = "Build Muscle" | "Lose Weight" | "Endurance" | "Mobility";
+type GoalSelectorProps = {
+  goals: Goal[];
+  selectedGoals: Goal[];
+  onToggleGoal: (goal: Goal) => void;
+};
+
+export default function GoalSelector({
+  goals,
+  selectedGoals,
+  onToggleGoal,
+}: GoalSelectorProps) {
   return (
     <>
       <h2 className="mt-2 text-3xl font-medium">1. What are your goals?</h2>
       <div className="grid grid-cols-2 gap-3 mt-8 mb-16">
-        {goals.map(goal => {
+        {goals.map((goal) => {
           const isSelected = selectedGoals.includes(goal);
 
           return (
@@ -13,8 +24,8 @@ function GoalSelector({ goals, selectedGoals, onToggleGoal }) {
               onClick={() => onToggleGoal(goal)}
               className={`p-4 rounded-xl border font-medium transition ${
                 isSelected
-                  ? 'bg-[var(--selected-color)] text-white dark:border-transparent'
-                  : 'bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-200 border-gray-300 dark:border-transparent hover:bg-gray-50'
+                  ? "bg-[var(--selected-color)] text-white dark:border-transparent"
+                  : "bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-200 border-gray-300 dark:border-transparent hover:bg-gray-50"
               }`}
             >
               {goal}
@@ -25,5 +36,3 @@ function GoalSelector({ goals, selectedGoals, onToggleGoal }) {
     </>
   );
 }
-
-export default GoalSelector
